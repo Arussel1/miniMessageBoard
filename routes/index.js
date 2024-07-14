7eafd4c87f82c5442bd1aee73c0881d995258140
Arussel <anhminh05102004@gmail.com>
2024-07-14 00:48:13 -0400

    Use MongoDB for saving messages

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,36 @@
 const express = require('express');
 const router = express.Router();
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
-];
+const Message = require('../models/message')
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: "FFA Chat Room", messages: messages });
+router.get('/', async function(req, res, next) {
+  try {
+    const messages = await Message.find().sort({ added: -1 }).exec();  // Fetch messages and sort by date
+    res.render('index', { title: 'FFA Chat Room', messages: messages });
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.get('/new', function(req, res, next) {
   res.render('form', { title: "Mini Messageboard" });
 });
 
-router.post('/new', (req, res, next) => {
-  messages.push({text: req.body.content, user: req.body.author, added: new Date()});
-  res.redirect('/')
-})
+router.post('/new', async (req, res, next) => {
+  try {
+    const newMessage = new Message({
+      name: req.body.author,
+      message: req.body.content,
+      added: new Date()
+    });
+    
+    await newMessage.save(); 
+
+    res.redirect('/');
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
